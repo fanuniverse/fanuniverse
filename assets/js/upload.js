da544c8745a970b89925b9e7c2246f2c3e9171af
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-07-03 21:51:00 +0700

    user avatars: views

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,8 +1,11 @@
 import { $ } from './utils/dom';
 
 export default function() {
-  const form = $('.js-upload');
-  form && (restoreForm() || setupForm());
+  const form = $('.js-upload'),
+        fancy = form && form.classList.contains('js-upload--image');
+
+  if (form && fancy) restoreForm() || setupForm(true);
+  else if (form) setupForm(false);
 }
 
 function restoreForm() {
@@ -11,16 +14,24 @@ function restoreForm() {
   return previousSubmission && proceedToNextStep();
 }
 
-function setupForm() {
-  $('.js-upload__fetch').addEventListener('click', scrapeUrl);
-
+function setupForm(fancy) {
   const reader = new FileReader();
 
   $('.file-upload__input').addEventListener('change', (e) =>
     e.target.files[0] && reader.readAsDataURL(e.target.files[0]));
 
-  reader.addEventListener('load', (e) =>
-    showImage(e.target.result) && proceedToNextStep());
+  reader.addEventListener('load', (e) => {
+    showImage(e.target.result);
+    fancy && proceedToNextStep();
+  });
+
+  if (fancy) $('.js-upload__fetch').addEventListener('click', scrapeUrl);
+}
+
+function showImage(src) {
+  const image = $('.js-upload__preview');
+  image.classList.remove('hidden');
+  image.src = src;
 
   return true;
 }
@@ -51,14 +62,6 @@ function toggleFetchButton() {
         oldState = button.textContent;
   button.textContent = button.getAttribute('data-toggle-text');
   button.setAttribute('data-toggle-text', oldState);
-}
-
-function showImage(src) {
-  const image = $('.js-upload__preview');
-  image.classList.remove('hidden');
-  image.src = src;
-
-  return true;
 }
 
 function insertScraped(data) {
