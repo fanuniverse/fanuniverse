7d841133583b6250d178051978af38c596c89fed
little-bobby-tables <ahawkthomas@gmail.com>
2017-07-08 20:31:27 +0700

    Repo.reload/1
    m'rails

--- lib/fanuniverse/repo.ex
+++ lib/fanuniverse/repo.ex
@@ -1,4 +1,8 @@
 defmodule Fanuniverse.Repo do
   use Ecto.Repo, otp_app: :fanuniverse
   use Pagination.Repo
+
+  def reload(%{id: id, __struct__: schema}), do: get(schema, id)
+
+  def reload!(%{id: id, __struct__: schema}), do: get!(schema, id)
 end
