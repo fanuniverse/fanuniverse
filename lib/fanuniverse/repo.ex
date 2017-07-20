54187a229417026a9d86806045b50c13b7e255d4
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-07-20 14:17:14 +0700

    Repo.count/1
    More Rails goodness

--- lib/fanuniverse/repo.ex
+++ lib/fanuniverse/repo.ex
@@ -2,7 +2,17 @@ defmodule Fanuniverse.Repo do
   use Ecto.Repo, otp_app: :fanuniverse
   use Pagination.Repo
 
+  import Ecto.Query
+
   def reload(%{id: id, __struct__: schema}), do: get(schema, id)
 
   def reload!(%{id: id, __struct__: schema}), do: get!(schema, id)
+
+  def count(queryable) do
+    queryable
+    |> exclude(:select)
+    |> exclude(:preload)
+    |> select([q], count(q.id))
+    |> one()
+  end
 end
