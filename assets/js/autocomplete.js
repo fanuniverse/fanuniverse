c0cef52dee2e6bbd2781e7bdd9fd53ae14bf9cca
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-08-01 10:44:54 +0700

    insert autocomplete suggestions

--- assets/js/autocomplete.js
+++ assets/js/autocomplete.js
@@ -1,35 +1,70 @@
-import { $, $$, removeChildren } from './utils/dom';
-import { afterLast } from './utils/text';
+import { $, $$, on, removeChildren } from './utils/dom';
+import { afterLast, escapeHtml } from './utils/text';
 
 let typingTimeout;
 
 export default function() {
-  $$('.js-autocomplete-target').forEach((field) =>
-    field.addEventListener('input', () => {
-      clearTimeout(typingTimeout);
-      typingTimeout = setTimeout(() => loadSuggestions(field), 200);
-    }));
+  $$('.js-autocomplete-target').forEach(setup);
+}
+
+function setup(field) {
+  const container = $('.js-autocomplete', field.parentNode);
+
+  on('click', '.js-autocomplete__line', (e, line) =>
+    insertSuggestion(field, line.textContent), container);
+
+  field.addEventListener('input', () => {
+    clearTimeout(typingTimeout);
+    typingTimeout = setTimeout(() => loadSuggestions(field), 200);
+  });
 }
 
 function loadSuggestions(field) {
-  const tag = (afterLast(field.value, ",") || field.value).trim();
-
-  fetch(`//client.lvh.me/autocomplete?q=${tag}`)
+  fetch(`//client.lvh.me/autocomplete?q=${typedTag(field.value)}`)
     .then((response) => response.json())
     .then((tags) => displaySuggestions(field, tags));
 }
 
 function displaySuggestions(field, tags) {
-  const container = $('.js-autocomplete', field.parentNode);
+  const container = $('.js-autocomplete', field.parentNode),
+        typed = typedTag(field.value);
 
   removeChildren(container);
 
-  tags.forEach(([name, score]) => {
+  tags.forEach(([tag, score]) => {
     const line = document.createElement('div');
 
     line.className = 'js-autocomplete__line';
-    line.textContent = name;
+    line.innerHTML = highlightTyped(tag, typed);
 
     container.appendChild(line);
   });
+}
+
+function insertSuggestion(field, tag) {
+  const insertAfter = field.value.lastIndexOf(typedTag(field.value));
+
+  field.value = field.value.substring(0, insertAfter) + tag + ', ';
+  field.focus();
+}
+
+function typedTag(text) {
+  return (afterLast(text, ",") || text).trim();
+}
+
+function highlightTyped(tag, typed) {
+  const typedPrefixes = typed.split(" "),
+        tagTerms = tag.split(" ");
+
+  return tagTerms.map((term) => {
+    const highlight = typedPrefixes.find((prefix) => term.startsWith(prefix)),
+          rest = highlight && term.slice(highlight.length);
+
+    if (highlight) {
+      return `<strong>${escapeHtml(highlight)}</strong>` + escapeHtml(rest);
+    }
+    else {
+      return escapeHtml(term);
+    }
+  }).join(' ');
 }
--- assets/js/utils/text.js
+++ assets/js/utils/text.js
@@ -4,3 +4,11 @@ export function afterLast(text, separator) {
   if (lastIndex === -1) return "";
   else return text.substr(lastIndex + 1);
 }
+
+/* Source: https://github.com/liamwhite/js-utils/blob/c578afeab2d867a3a3cd7b272a03a3d1b82370bc/dom.js#L64 */
+export function escapeHtml(text) {
+  return text.replace(/&/g, '&amp;')
+             .replace(/>/g, '&gt;')
+             .replace(/</g, '&lt;')
+             .replace(/"/g, '&quot;');
+}
