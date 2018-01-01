4ad0a3822b3ba08727b30539abccf5c374a823cb
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2018-01-01 12:43:11 +0700

    :arrow_up: frontend deps

--- assets/rollup-vendor.js
+++ assets/rollup-vendor.js
@@ -7,7 +7,7 @@ const production = process.env.NODE_ENV === "production";
 export default {
   input: [
     './node_modules/whatwg-fetch/fetch.js',
-    './node_modules/promise-polyfill/promise.js',
+    './node_modules/promise-polyfill/dist/promise.js',
     './node_modules/element-closest/element-closest.js',
     './node_modules/textarea-caret/index.js',
     './node_modules/phoenix_html/priv/static/phoenix_html.js'
